0fe166fe8983e5f96c94185228d05c4a2b3c00d0
Fauzan <fauzantahir660@gmail.com>
2025-04-16 18:28:43 +0500

    Basic All Auth

--- Resume/urls.py
+++ Resume/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from notes.views import members
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('members/', members)
+    path('members/', members),
+    path('accounts/', include('allauth.urls')),
 ]
