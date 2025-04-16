c4368242b1487b00e5d85569b121d16c756ad856
Fauzan <fauzantahir660@gmail.com>
2025-04-16 10:40:13 +0500

    Basic Setup

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import HttpResponse
+from django.template import loader
 # Create your views here.
+def members(request):
+    template = loader.get_template('notes/resume.html')
+    return HttpResponse(template.render())
